1d2397cb4575b3b6eaf449a5166664e5d09074de
Rolf <rolf.benschop@monkeyproofsolutions.nl>
2024-08-16 18:18:57 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -8,10 +8,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 f = open('.simian.json') 
 simian_info = json.load(f) 
+print(simian_info)
 
-@app.function_name(name="http_trigger")
+@app.function_name(name="HttpTrigger1")
 @app.route(route={name})
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
    
