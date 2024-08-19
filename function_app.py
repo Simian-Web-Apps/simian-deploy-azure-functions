91cdf26c5973e96087e86bd3c979017900ab0476
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-19 10:12:07 +0200

    new compacter code per:
    https://doc.simiansuite.com/simian-gui/deployment/python.html#azure-functions

--- function_app.py
+++ function_app.py
@@ -19,55 +19,13 @@ for ns in simian_info["route-namespace-map"]:
 
 # route is the part after api/ and we call it api_slug
 @app.route(route="{api_slug}")
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-   
-    try:
-        request_data = req.get_json()
-    except ValueError as exc:
-        return func.HttpResponse(
-            _create_error_response(
-                "An error occurred when processing the request data: " + str(exc), exc
-            ),
-            status_code=200,
-        )
-
     # Route the post to the entrypoint method.
     api_slug = req.route_params.get("api_slug")
     namespace = namespaces[api_slug]["namespace"]
-    request_data[1].update({"namespace": namespace})
+    
+    request_data = req.get_json()
+    response = entry_point_deploy(namespace, request_data)
 
-    try:
-        # Call the entry_point to access the application with the request data.
-        payload_out = entry_point(
-            request_data[0], # operation
-            request_data[1], # metadata
-            request_data[2], # payload
-        )
-
-        # Defer loading the utils until the entry_point has checked that Simian GUI
-        # can be used.
-        from simian.gui import utils
-
-        # Return the payload as a JSON string to Azure.
-        response = f'{{"returnValue": {utils.payloadToString(payload_out)}}}'
-
-    except Exception as exc:
-        # Put the error message in the response.
-        logging.error("Error caught by entrypoint wrapper: %r", exc)
-        response = _create_error_response(str(exc), exc)
-
-    return func.HttpResponse(response, status_code=200)
-
-
-def _create_error_response(msg: str, exc: Exception) -> dict:
-    """Create an error response dictionary from a caught exception."""
-    return json.dumps(
-        {
-            "error": {
-                "message": msg,
-                "stacktrace": traceback.format_tb(exc.__traceback__),  # Optional
-            }
-        }
-    )
+    return func.HttpResponse(json.dumps(response), status_code=200)
