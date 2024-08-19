971534c66166cf44adc300f3562201af8622bc38
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-19 15:39:17 +0200

    simplify
    - reduce to a single app, delete modules and simian.json
    - define route and namespace in function_app.py

--- function_app.py
+++ function_app.py
@@ -1,17 +1,20 @@
 import json
-import os
 
 import azure.functions as func
 from simian.entrypoint import entry_point_deploy
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
-
+# Define simian app:
+# - route without azure function prefix (default: api/), 
+# - namespace of its module
 simian_app = {
     "route": "hello-world",
-    "namespace": "apps.hello_world"
+    "namespace": "app.hello_world"
 }
 
-# route is the part after the prefix (default: api/)
+# Anonymous access for symplicity
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+# Route is the part after the prefix (default: api/)
 @app.route(route=simian_app["route"])
 def main(req: func.HttpRequest) -> func.HttpResponse:
     request_data = req.get_json()
