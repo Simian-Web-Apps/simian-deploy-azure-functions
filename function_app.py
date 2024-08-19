e4c1df6b225c0d9a6faee87b186bd2a117bc8cf3
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-19 11:02:40 +0200

    forgotten column on def

--- function_app.py
+++ function_app.py
@@ -7,20 +7,18 @@ from simian.entrypoint import entry_point_deploy
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-    
 # route is the part after the prefix (default: api/)
 @app.route(route="{api_slug}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Route the post to the entrypoint method.
-    namespace = get_namespace(req.route_params.get("api_slug"))
-    
+    namespace = get_namespace_by_slug(req.route_params.get("api_slug"))
     request_data = req.get_json()
     response = entry_point_deploy(namespace, request_data)
 
     return func.HttpResponse(json.dumps(response), status_code=200)
 
 
-def get_namespace(api_slug: str) -> str
+def get_namespace_by_slug(api_slug: str) -> str:
     # simian.json contains simian specific app info
     # a.o. the route to module namespace mapping
     simian_json_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'simian.json')
@@ -34,5 +32,3 @@ def get_namespace(api_slug: str) -> str
     namespace = namespaces[api_slug]["namespace"]
     
     return namespace
-    
-    
