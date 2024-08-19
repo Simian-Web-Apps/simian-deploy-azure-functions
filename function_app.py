f7dcf481eb19a00ea5dd658b2a724b7263b2c1e5
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-19 17:21:23 +0200

    simplify

--- function_app.py
+++ function_app.py
@@ -1,24 +1,21 @@
+# see: https://doc.simiansuite.com/simian-gui/deployment/python.html#azure-functions
 import json
 from http import HTTPStatus
 
 import azure.functions as func
 from simian.entrypoint import entry_point_deploy
 
-# Define simian app:
-# - route without azure function prefix (default: api/),
-# - namespace of its module
-simian_app_base_data = {"route": "hello-world-step-", "namespace": "apps.hello_world_step_"}
-
 # Anonymous access for symplicity
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
 # Route is the part after the prefix (default: api/)
-@app.route(route=simian_app_base_data["route"] + "{step:int}")
+# Define the route with a numeric parameter "step" to facilitate multiple hello-world steps
+@app.route(route="hello-world-step-{step:int}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     request_data = req.get_json()
-    response = entry_point_deploy(
-        simian_app_base_data["namespace"] + req.route_params.get("step"), request_data
-    )
+    # Compose the simian app module namespace from base namespace and route parameter "step".
+    app_module_namespace = "apps.hello_world_step_" + req.route_params.get("step")
+    response = entry_point_deploy(app_module_namespace, request_data)
 
     return func.HttpResponse(json.dumps(response), status_code=HTTPStatus.OK)
