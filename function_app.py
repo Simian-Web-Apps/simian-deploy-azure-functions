d4e2f6f3601ab171d6f85e27d501eae9864fa979
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-08-19 16:48:21 +0200

    fix variable name

--- function_app.py
+++ function_app.py
@@ -14,11 +14,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
 # Route is the part after the prefix (default: api/)
-@app.route(route=simian_app["route"] + "{step:int}")
+@app.route(route=simian_app_base_data["route"] + "{step:int}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     request_data = req.get_json()
     response = entry_point_deploy(
-        simian_app["namespace"] + req.route_params.get("step"), request_data
+        simian_app_base_data["namespace"] + req.route_params.get("step"), request_data
     )
 
     return func.HttpResponse(json.dumps(response), status_code=HTTPStatus.OK)
