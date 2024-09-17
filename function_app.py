ea172e31445cdc36e13d5e3579e8702ae952d34e
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-09-17 15:59:54 +0200

    simplify

--- function_app.py
+++ function_app.py
@@ -15,7 +15,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     request_data = req.get_json()
     # Compose the simian app module namespace from base namespace and route parameter "step".
-    app_module_namespace = "apps.hello_world_step_" + req.route_params.get("step")
+    app_module_namespace = "apps." + req.route_params.get("app")
     response = entry_point_deploy(app_module_namespace, request_data)
 
     return func.HttpResponse(json.dumps(response), status_code=HTTPStatus.OK)
