54d0ec51f46dc161ca4855a15cb99b009e458bde
Rolf Benschop <rolf.benschop@MONKEYPROOF.LAN>
2024-09-17 15:48:52 +0200

    simplify

--- function_app.py
+++ function_app.py
@@ -11,11 +11,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # Route is the part after the prefix (default: api/)
 # Define the route with a numeric parameter "step" to facilitate multiple hello-world steps
-@app.route(route="{app:str}")
+@app.route(route="{app}")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     request_data = req.get_json()
     # Compose the simian app module namespace from base namespace and route parameter "step".
-    app_module_namespace = "apps." + req.route_params.get("app")
+    app_module_namespace = "apps.hello_world_step_" + req.route_params.get("step")
     response = entry_point_deploy(app_module_namespace, request_data)
 
     return func.HttpResponse(json.dumps(response), status_code=HTTPStatus.OK)
